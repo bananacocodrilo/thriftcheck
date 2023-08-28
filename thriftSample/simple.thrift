7f014277098058f13a9ba9b76e5ed529e4ded729
bananacocodrilo <antonio.mkp@gmail.com>
2023-08-28 14:27:21 +0200

    Add sample file to visualize the new checks

---
+++ thriftSample/simple.thrift
@@ -0,0 +1,30 @@
+ 
+namespace java com.bananacocodrilo.simple.service
+
+enum ImplicitValues {
+  ZERO,
+  ONE,
+  TWO,
+}  
+
+enum ImplicitValuesWithNegative {
+  UNKNOWN =  -1,
+  ZERO,
+  ONE,
+  TWO,
+}  
+
+enum ExplicitValues {
+  ZERO = 0,
+  ONE = 1,
+  TWO = 2,
+}  
+ 
+enum ExplicitValuesWithNegative {
+  UNKNOWN =  -1,
+  ZERO = 0,
+  ONE = 1,
+  TWO = 2,
+}  
+ 
+  
